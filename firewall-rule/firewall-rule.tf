6911221b6f4da14f5f00b5e55fa42f57ed053bfc
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-03 16:19:32 +0200

    a

--- firewall-rule/firewall-rule.tf
+++ firewall-rule/firewall-rule.tf
@@ -5,8 +5,8 @@ resource "google_compute_firewall" "firewall-rule" {
 
   allow {
     protocol = "${var.protocol}"
-    ports    = var.ports
+    ports    = ["${var.ports}"]
   }
-  target_tags   = var.tags
-  source_ranges = var.source_ranges
+  target_tags   = ["${var.tags}"]
+  source_ranges = ["${var.source_ranges}"]
 }
