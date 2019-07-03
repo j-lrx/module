6f3e2f8a01aa52f106f3555dc5f8ba3717d9c08f
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-03 16:21:46 +0200

    a

--- firewall-rule/firewall-rule.tf
+++ firewall-rule/firewall-rule.tf
@@ -4,9 +4,9 @@ resource "google_compute_firewall" "firewall-rule" {
   description = "Creates Firewall rule targetting tagged instances"
 
   allow {
-    protocol = "${var.protocol}"
-    ports    = ["${var.ports}"]
+    protocol = var.protocol
+    ports    = var.ports
   }
-  target_tags   = ["${var.tags}"]
-  source_ranges = ["${var.source_ranges}"]
+  target_tags   = var.tags
+  source_ranges = var.source_ranges
 }
