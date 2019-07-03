8669596477bb031718ad7808b2c5412fa89db6de
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-03 14:48:24 +0200

    a

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc"{
     name                     = "${var.name}"
     project                  = "${var.project_id}"
-    auto_create_subnectworks = false
+    auto_create_subnetworks = false
 }
