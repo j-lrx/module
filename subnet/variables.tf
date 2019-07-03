96174ea1bad6419a89ddf29078fe2886f7a5bf79
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-03 14:27:09 +0200

    a

--- subnet/variables.tf
+++ subnet/variables.tf
@@ -4,6 +4,11 @@ variable "name" {
   type = string
 }
 
+variable "project_id" {
+  type = string
+}
+
+
 variable "cicr_range" {
   type = string
 }
@@ -12,16 +17,12 @@ variable "network_id" {
   type = string
 }
 
-variable "enable_flow_logs" {
-  type    = bool
-  default = false
-}
-
 variable "region" {
   type    = string
   default = "europe-west1"
 }
 
-variable "project_id" {
-  type = string
+variable "enable_flow_logs" {
+  type    = bool
+  default = false
 }
