58dd7cdc349a152cae5dfb261f49ba7a603ac28b
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-18 17:11:24 +0200

    a

--- basic/container_cluster/container_cluster.tf
+++ basic/container_cluster/container_cluster.tf
@@ -5,12 +5,11 @@ resource "google_container_cluster" "container_cluster" {
     network                     = var.network
     subnetwork                  = var.subnetwork
 
-    addons_config {
-        network_policy {
-            enabled                 = true
-            provider                = "CALICO"
-        }
+    network_policy {
+        enabled                 = true
+        provider                = "CALICO"
     }
+
 
     node_pool {
         name               = "pool"
@@ -30,15 +29,5 @@ resource "google_container_cluster" "container_cluster" {
             preemptible  = var.preemptible
             machine_type = var.machine_type
         }
-
-        oauth_scopes = [
-        "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/trace.append",
-        "https://www.googleapis.com/auth/compute",
-      ]
     }
 }
