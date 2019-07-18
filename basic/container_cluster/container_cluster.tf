e155c040c3ecd24b3966b2799816ec327a7ceb89
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-18 17:43:45 +0200

    a

--- basic/container_cluster/container_cluster.tf
+++ basic/container_cluster/container_cluster.tf
@@ -10,6 +10,17 @@ resource "google_container_cluster" "container_cluster" {
         provider                = "CALICO"
     }
 
+    cluster_autoscaling {
+        enabled = true
+        resource_limits {
+            resource_type = "cpu"
+            minimum = 3
+        }
+        resource_limits {
+            resource_type = "memory"
+            minimum = 11
+      }
+    }
 
     node_pool {
         name               = "pool"
