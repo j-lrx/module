a1e723930cc856aa2cf5851b73c671618925f333
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-18 14:50:19 +0200

    a

--- basic/container_node_pool/output.tf
+++ basic/container_node_pool/output.tf
@@ -1,7 +1,3 @@
 output "name" {
   value = "${google_container_node_pool.container_node_pool.name}"
 }
-
-output "self_link" {
-  value = "${google_container_node_pool.container_node_pool.self_link}"
-}
