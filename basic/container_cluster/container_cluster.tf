d6e4688c08784c1ace98af45e9e586a67515ada8
julien-le-roux <49448040+julien-le-roux@users.noreply.github.com>
2019-07-19 17:54:35 +0200

    a

--- basic/container_cluster/container_cluster.tf
+++ basic/container_cluster/container_cluster.tf
@@ -1,14 +1,13 @@
 resource "google_container_cluster" "container_cluster" {
-    name                        = var.name
-    location                    = var.location
-    project                     = var.project
-    network                     = var.network
-    subnetwork                  = var.subnetwork
-    min_master_version          = "latest"
-    cluster_ipv4_cidr           = var.cluster_ipv4_cidr
+    name                         = var.name
+    location                     = var.location
+    project                      = var.project
+    network                      = var.network
+    subnetwork                   = var.subnetwork
+    min_master_version           = "latest"
 
-    remove_default_node_pool    = true
-    initial_node_count          = 1
+    remove_default_node_pool     = true
+    initial_node_count           = 1
 
     addons_config {
         network_policy_config {
@@ -25,7 +24,14 @@ resource "google_container_cluster" "container_cluster" {
     }
 
     network_policy {
-        enabled                 = true
-        provider                = "CALICO"
+        enabled                  = true
+        provider                 = "CALICO"
+    }
+
+    ip_allocation_policy {
+        node_ipv4_cidr_block     = var.node_ipv4_cidr
+        cluster_ipv4_cidr_block  = var.cluster_ipv4_cidr
+        services_ipv4_cidr_block = var.services_ipv4_cidr
+       
     }
 }
--- basic/container_cluster/variables.tf
+++ basic/container_cluster/variables.tf
@@ -18,6 +18,14 @@ variable "subnetwork" {
   type = string
 }
 
+variable "node_ipv4_cidr" {
+  type = string
+}
+
 variable "cluster_ipv4_cidr" {
+  type = string
+}
+
+variable "services_ipv4_cidr" {
   type = string
 }
