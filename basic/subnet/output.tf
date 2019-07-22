d49b46f7fb7485654474344976f719ed6cb812e7
j-lrx <49448040+j-lrx@users.noreply.github.com>
2019-07-22 15:37:55 +0200

    Update output.tf

--- basic/subnet/output.tf
+++ basic/subnet/output.tf
@@ -1,3 +1,8 @@
 output "self_link" {
   value = "${google_compute_subnetwork.subnet.self_link}"
 }
+
+output "gateway_address" {
+  value = "${google_compute_subnetwork.subnet.gateway_address}"
+}
+
