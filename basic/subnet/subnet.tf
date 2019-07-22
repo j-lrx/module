331338476ffb75969d748fe73c2e704286f12ef5
j-lrx <49448040+j-lrx@users.noreply.github.com>
2019-07-22 15:32:45 +0200

    Update subnet.tf

--- basic/subnet/subnet.tf
+++ basic/subnet/subnet.tf
@@ -1,8 +1,8 @@
 resource "google_compute_subnetwork" "subnet"{
     name             = "${var.name}"
-    project          = "${var.project_id}"
-    ip_cidr_range    = "${var.cidr_range}"
-    network          = "${var.network_id}"
-    enable_flow_logs = "${var.enable_flow_logs}"
+    project          = "${var.project}"
+    network          = "${var.network}"
     region           = "${var.region}"
+    ip_cidr_range    = "${var.cidr_range}"
+    enable_flow_logs = "${var.enable_flow_logs}"
 }
