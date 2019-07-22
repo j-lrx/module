7f3c69363e6401b0576db8435bdb108efcb6e952
j-lrx <49448040+j-lrx@users.noreply.github.com>
2019-07-22 15:46:54 +0200

    Update vpc.tf

--- basic/vpc/vpc.tf
+++ basic/vpc/vpc.tf
@@ -2,4 +2,5 @@ resource "google_compute_network" "vpc"{
     name                     = "${var.name}"
     project                  = "${var.project_id}"
     auto_create_subnetworks  = "${var.auto_create_subnetworks}"
+    description              = "${var.description}"
 }
