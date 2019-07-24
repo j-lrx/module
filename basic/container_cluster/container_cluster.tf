03885cea80b76e728fc7db2d46018eca5c6b2a80
j-lrx <49448040+j-lrx@users.noreply.github.com>
2019-07-24 10:42:01 +0200

    Update container_cluster.tf

--- basic/container_cluster/container_cluster.tf
+++ basic/container_cluster/container_cluster.tf
@@ -7,6 +7,7 @@ resource "google_container_cluster" "container_cluster" {
     min_master_version           = "latest"
 
     remove_default_node_pool     = true
+    initial_node_count           = 1
 
     addons_config {
         network_policy_config {
