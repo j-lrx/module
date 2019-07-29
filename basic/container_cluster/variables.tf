90b00474d11f8bcceb327d8d77ddd8e6d5f70899
j-lrx <49448040+j-lrx@users.noreply.github.com>
2019-07-29 10:28:33 +0200

    Update variables.tf

--- basic/container_cluster/variables.tf
+++ basic/container_cluster/variables.tf
@@ -6,6 +6,7 @@ variable "name" {
 variable "location" {
   type    = string
 }
+
 variable "project" {
   type = string
 }
@@ -28,4 +29,39 @@ variable "cluster_ipv4_cidr" {
 
 variable "services_ipv4_cidr" {
   type = string
+}
+
+variable "node_pool_name"{
+  type    = string
+  default = "default-node-pool"
+}
+
+variable "min_node_count"{
+  type    = number
+  default = 1
+}
+
+variable "max_node_count"{
+  type    = number
+  default = 3
+}
+
+variable "machine-type"{
+  type    = string
+  default = "n1-standard-1"
+}
+
+variable "preemptible"{
+  type    = bool
+  default = false
+}
+
+variable "auto_repair"{
+  type    = bool
+  default = true
+}
+
+variable "auto_upgrade"{
+  type    = bool
+  default = false
 }
