cd8e99ebe0f7a252e59576c73a110a341662f89b
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-01-20 12:06:44 +0100

    Update private_vpc_connection.tf

--- basic/private_vpc_connection/private_vpc_connection.tf
+++ basic/private_vpc_connection/private_vpc_connection.tf
@@ -2,5 +2,5 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 
   network                 = var.network
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = var.private_ip_address
+  reserved_peering_ranges = [var.private_ip_address]
 }
