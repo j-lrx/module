0bfb8c91f12f929e449b377294c96211fefe1733
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-01-20 12:14:01 +0100

    Update cloud_sql.tf

--- basic/cloud_sql/cloud_sql.tf
+++ basic/cloud_sql/cloud_sql.tf
@@ -1,3 +1,10 @@
+resource "google_service_networking_connection" "private_vpc_connection" {
+
+  network                 = var.network
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [var.private_ip_address]
+}
+
 resource "google_sql_database_instance" "sql_db" {
   provider = google-beta
   name             = var.name
