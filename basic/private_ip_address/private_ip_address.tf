3a16b87a82b069552d384a958b906d3187418df5
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-01-20 10:45:27 +0100

    Update private_ip_address.tf

--- basic/private_ip_address/private_ip_address.tf
+++ basic/private_ip_address/private_ip_address.tf
@@ -5,5 +5,6 @@ resource "google_compute_global_address" "private_ip_address" {
   purpose       = var.purpose
   address_type  = var.address_type
   prefix_length = 16
-  network       = google_compute_network.private_network.id
+  network       = var.network
+  ip_version    = var.ip_version
 }
