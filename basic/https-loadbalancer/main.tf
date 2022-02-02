3e751376cbde66930bc4c053643a5a409c08ebdb
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-02 16:56:26 +0100

    Create main.tf

---
+++ basic/https-loadbalancer/main.tf
@@ -0,0 +1,28 @@
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "ssl-proxy-xlb-forwarding-rule"
+  provider              = google
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "443"
+  target                = google_compute_target_ssl_proxy.default.id
+  ip_address            = google_compute_global_address.default.id
+}
+
+resource "google_compute_global_address" "default" {
+  name = "lb-static-ip"
+}
+
+resource "google_compute_target_ssl_proxy" "default" {
+  name             = "test-proxy"
+  backend_service  = var.google_compute_backend_service.default.id            # A remplacer par le backend
+  ssl_certificates = ["google_compute_managed_ssl_certificate.default.id"]
+}
+
+resource "google_compute_managed_ssl_certificate" "default" {
+  name = "test-cert"
+
+  managed {
+    domains = ["sslcert.tf-test.club."]
+  }
+}
+
